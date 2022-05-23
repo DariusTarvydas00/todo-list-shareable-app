9da98f69e5c62bbf7505d43e26af1a25569c1a60
DariusTarvydas <tarvydasdarius00@gmail.com>
2022-05-23 15:20:30 +0300

    test

--- todo-list-shareable-frontend/src/registerServiceWorker.ts
+++ todo-list-shareable-frontend/src/registerServiceWorker.ts
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === "production") {
       );
     },
     registered() {
-      console.log("Service worker has been registered.");
+      console.log("Servicse worker has been registered.");
     },
     cached() {
       console.log("Content has been cached for offline use.");
