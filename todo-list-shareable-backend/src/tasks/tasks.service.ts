2156e565538ce82398a373301b2d3d8ca116843f
DariusTarvydas <tarvydasdarius00@gmail.com>
2022-05-24 08:34:05 +0300

    test

--- todo-list-shareable-backend/src/tasks/tasks.service.ts
+++ todo-list-shareable-backend/src/tasks/tasks.service.ts
@@ -20,4 +20,7 @@ export class TasksService {
         this.tasks.push(task);
         return task;
     }
+    getTaskById(id:string): Task {
+        return this.tasks.find((task) => task.id === id);
+    }
 }
