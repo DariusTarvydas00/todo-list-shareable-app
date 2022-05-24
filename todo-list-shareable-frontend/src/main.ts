7a9bfc74409f8441308b85b98f13088f6b5c4b5c
DariusTarvydas <tarvydasdarius00@gmail.com>
2022-05-25 01:05:53 +0300

    test

--- todo-list-shareable-frontend/src/main.ts
+++ todo-list-shareable-frontend/src/main.ts
@@ -3,5 +3,8 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import { createPinia } from 'pinia'
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App)
+app.config.globalProperties.hostname = "http://161.97.99.214:3000"
+app.use(store).use(router).use(createPinia()).mount("#app");
--- todo-list-shareable-frontend/src/router/index.ts
+++ todo-list-shareable-frontend/src/router/index.ts
@@ -1,11 +1,23 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import Login from "../views/LoginView.vue";
+import Register from "../views/RegisterView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: Login,
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: Register,
   },
   {
     path: "/about",
@@ -19,8 +31,20 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(),
   routes,
 });
+router.beforeEach((to, from, next) => {
+  const publicPages = ['/login', '/register'];
+  const authRequired = !publicPages.includes(to.path);
+  const loggedIn = localStorage.getItem('token');
 
+  //@todo Confirm token here on backend?
+
+  if (authRequired && !loggedIn) {
+    next('/login');
+  } else {
+    next();
+  }
+});
 export default router;
