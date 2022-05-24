dfdfa2eaa1d918a571aacc385c6c6f5c7f60ff8a
DariusTarvydas <tarvydasdarius00@gmail.com>
2022-05-24 11:59:07 +0300

    test

--- todo-list-shareable-backend/src/tasks/tasks.controller.ts
+++ todo-list-shareable-backend/src/tasks/tasks.controller.ts
@@ -23,16 +23,16 @@ export class TasksController {
     getTaskById(@Param('id') id: string): Promise<Task>{
         return this.tasksService.getTaskById(id);
     }
-    //
+
     @Post()
     createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
         return this.tasksService.createTask(createTaskDto);
     }
-    //
-    // @Delete('/:id')
-    // deleteTask(@Param('id')id: string): void {
-    //     return this.tasksService.deleteTask(id);
-    // }
+
+    @Delete('/:id')
+    deleteTask(@Param('id')id: string): Promise<void> {
+        return this.tasksService.deleteTask(id);
+    }
     //
     // @Patch('/:id/status')
     // updateTaskStatus(
