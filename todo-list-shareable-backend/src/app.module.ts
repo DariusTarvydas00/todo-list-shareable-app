a87d3fb4817ab8ebfba5a10e258cc4c091e51de8
DariusTarvydas <tarvydasdarius00@gmail.com>
2022-05-24 11:17:58 +0300

    test

--- todo-list-shareable-backend/src/app.module.ts
+++ todo-list-shareable-backend/src/app.module.ts
@@ -3,12 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import {TypeOrmModule} from "@nestjs/typeorm";
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [TasksModule,TypeOrmModule.forRoot({
+  imports: [TasksModule,
+      ConfigModule.forRoot({isGlobal:true}),
+    TypeOrmModule.forRoot({
     type: 'postgres',
+    url: process.env.DATABASE_URL,
     host: 'nestjs_backend',
     port: 5432,
     username: 'postgres',
