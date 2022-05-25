1543dd459fa6cfca5619a881bebe01da6355aaad
DariusTarvydas <tarvydasdarius00@gmail.com>
2022-05-25 11:08:47 +0300

    test

--- todo-list-shareable-backend/src/auth/auth.module.ts
+++ todo-list-shareable-backend/src/auth/auth.module.ts
@@ -3,11 +3,21 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {UsersRepository} from "./users.repository";
+import {PassportModule} from "@nestjs/passport";
+import {JwtModule} from '@nestjs/jwt'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([
-      UsersRepository
-  ])],
+  imports: [
+      PassportModule.register({
+          defaultStrategy: 'jwt'
+      }),
+      JwtModule.register({
+          secret: 'secret',
+          signOptions: {
+              expiresIn: 3600
+          }
+      }),
+      TypeOrmModule.forFeature([UsersRepository])],
   controllers: [AuthController],
   providers: [AuthService]
 })
