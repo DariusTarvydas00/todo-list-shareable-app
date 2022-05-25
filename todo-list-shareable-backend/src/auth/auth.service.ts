7f62aabb4b30e0c93be039ff6ec9364b1fc4e496
DariusTarvydas <tarvydasdarius00@gmail.com>
2022-05-25 09:53:50 +0300

    test

--- todo-list-shareable-backend/src/auth/auth.service.ts
+++ todo-list-shareable-backend/src/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import {UserRepository} from "./user.repository";
 import {InjectRepository} from "@nestjs/typeorm";
-import {AuthCredentialsDto} from "./dto/auth.credentials.dto";
+import {AuthCredentialsDto} from "./dto/auth-credentials.dto";
 
 @Injectable()
 export class AuthService {
