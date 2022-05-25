b1ba15e81d8821d3509c75c632d4d3e108b5937c
DariusTarvydas <tarvydasdarius00@gmail.com>
2022-05-25 17:07:19 +0300

    test

--- todo-list-shareable-backend/src/app.module.ts
+++ todo-list-shareable-backend/src/app.module.ts
@@ -1,36 +1,42 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {ConfigModule, ConfigService} from "@nestjs/config";
 import { AuthModule } from './auth/auth.module';
 import {configValidationSchema} from "./config.schema";
 
+const ENV = process.env.NODE_ENV;
+
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
-  imports: [TasksModule,
-      ConfigModule.forRoot({
-          envFilePath: ['.env.stage.${process.env.STAGE}'],
-          validationSchema: configValidationSchema,
-          isGlobal:true
-      }),
-      TypeOrmModule.forRootAsync({
-        imports: [ConfigModule],
-        inject: [ConfigService],
-        useFactory: async (configService: ConfigService) => ({
-            type: 'postgres',
-            autoLoadEntities: true,
-            synchronize: true,
-            host: configService.get('DB_HOST'),
-            port: configService.get('DB_PORT'),
-            username: configService.get('DB_USERNAME'),
-            password: configService.get('DB_PASSWORD'),
-            database: configService.get('DB_DATABASE'),
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: [`./environments/.env.stage.${process.env.STAGE}`],
+            validationSchema: configValidationSchema,
         }),
-      }),
-    AuthModule,
-  ],
+        TasksModule,
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) => {
+                const isProduction = configService.get('STAGE') === 'prod';
+
+                return {
+                    ssl: isProduction,
+                    extra: {
+                        ssl: isProduction ? { rejectUnauthorized: false } : null,
+                    },
+                    type: 'postgres',
+                    autoLoadEntities: true,
+                    synchronize: true,
+                    host: configService.get('DB_HOST'),
+                    port: configService.get('DB_PORT'),
+                    username: configService.get('DB_USERNAME'),
+                    password: configService.get('DB_PASSWORD'),
+                    database: configService.get('DB_DATABASE'),
+                };
+            },
+        }),
+        AuthModule,
+    ],
 })
 export class AppModule {}
--- todo-list-shareable-backend/src/config.schema.ts
+++ todo-list-shareable-backend/src/config.schema.ts
@@ -1,8 +1,9 @@
 import * as Joi from '@hapi/joi'
 
 export const configValidationSchema = Joi.object({
-    STAGE: Joi.string().required(),
+    PORT: Joi.number().default(3000),
     DB_HOST: Joi.string().required(),
+    STAGE: Joi.string().required(),
     DB_PORT: Joi.number().default(5432).required(),
     DB_USERNAME: Joi.string().required(),
     DB_PASSWORD: Joi.string().required(),
