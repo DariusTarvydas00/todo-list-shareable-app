32ac4daa783830cc469c913d0a8af8fb57b1b8e8
DariusTarvydas <tarvydasdarius00@gmail.com>
2022-05-26 14:45:32 +0300

    test

--- todo-list-shareable-backend/src/main.ts
+++ todo-list-shareable-backend/src/main.ts
@@ -3,12 +3,21 @@ import { AppModule } from './app.module';
 import {ValidationPipe} from "@nestjs/common";
 import {TransformInterceptor} from "./transform.interceptor";
 import { Logger } from '@nestjs/common'
+import {config} from "rxjs";
 
 async function bootstrap() {
   const logger = new Logger();
-  const app = await NestFactory.create(AppModule, {
-    cors: {origin: '*', allowedHeaders: '*'}
-  });
+  const app = await NestFactory.create(AppModule)
+  if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
+    app.enableCors()
+  } else {
+    app.enableCors({
+      origin: 'http://161.97.99.214:3000'
+      // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      // credentials: false,
+      // ...
+    })
+  }
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3000);
