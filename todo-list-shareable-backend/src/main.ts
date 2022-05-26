729835f62a2bf8aa13cb4b38eb82ec228f03262c
DariusTarvydas <tarvydasdarius00@gmail.com>
2022-05-26 19:22:52 +0300

    test

--- todo-list-shareable-backend/src/main.ts
+++ todo-list-shareable-backend/src/main.ts
@@ -8,11 +8,14 @@ import { cors } from 'cors'
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule, {cors: true});
-  app.enableCors({
-    origin: 'http://nestjs_backend'
-  });
+  const server = require('http').createServer();
+  const options={
+    cors:true,
+    origins:["http://127.0.0.1:3000","http://localhost:3000","http://169.97.99.214","http://nestjs_backend"],
+  }
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
+  app.use(cors(options))
   await app.listen(3000);
   const port = 3000;
   logger.log('Application listening  on port ${port}')
