27adc56b6e2498562549d2b7f3a4a046cc283885
DariusTarvydas <tarvydasdarius00@gmail.com>
2022-05-26 14:21:21 +0300

    test

--- todo-list-shareable-backend/src/main.ts
+++ todo-list-shareable-backend/src/main.ts
@@ -3,10 +3,22 @@ import { AppModule } from './app.module';
 import {ValidationPipe} from "@nestjs/common";
 import {TransformInterceptor} from "./transform.interceptor";
 import { Logger } from '@nestjs/common'
+import {CorsOptions} from "@nestjs/common/interfaces/external/cors-options.interface";
 
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
+  const corsOptions: CorsOptions = {
+    allowedHeaders: ['origin', 'x-requested-with', 'content-type', 'accept', 'authorization'],
+    credentials: true,
+    origin: ['http://localhost:3001', 'http://localhost:3000'],
+  }
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  app.enableCors(corsOptions);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3000);
