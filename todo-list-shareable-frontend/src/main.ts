5decbb101ce8013f1860dba9590994a5e205caae
DariusTarvydas <tarvydasdarius00@gmail.com>
2022-05-26 10:43:58 +0300

    test

--- todo-list-shareable-frontend/src/main.ts
+++ todo-list-shareable-frontend/src/main.ts
@@ -6,5 +6,5 @@ import store from "./store";
 import { createPinia } from 'pinia'
 
 const app = createApp(App)
-app.config.globalProperties.hostname = "http://161.97.99.214:3000"
+app.config.globalProperties.hostname = "http://localhost:3000"
 app.use(store).use(router).use(createPinia()).mount("#app");
