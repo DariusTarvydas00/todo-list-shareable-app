a2ea690454fb653f589c6cb5d6c56eceacd9111c
DariusTarvydas <tarvydasdarius00@gmail.com>
2022-05-26 10:38:05 +0300

    test

--- todo-list-shareable-frontend/vue.config.js
+++ todo-list-shareable-frontend/vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+    devServer: {
+      proxy: 'http://161.97.99.214:3000'
+    }
 });
