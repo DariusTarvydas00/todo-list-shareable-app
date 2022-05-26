47d78887f5e17f70addd86d72ba723f9d8ec0b54
DariusTarvydas <tarvydasdarius00@gmail.com>
2022-05-26 17:28:15 +0300

    test

--- todo-list-shareable-backend/src/main.ts
+++ todo-list-shareable-backend/src/main.ts
@@ -3,11 +3,13 @@ import { AppModule } from './app.module';
 import {ValidationPipe} from "@nestjs/common";
 import {TransformInterceptor} from "./transform.interceptor";
 import { Logger } from '@nestjs/common'
+import { cors } from 'cors'
 
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(cors())
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3000);
