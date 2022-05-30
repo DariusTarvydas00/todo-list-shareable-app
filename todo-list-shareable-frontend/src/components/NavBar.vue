68ab5c7963699aa7e2740370ed9b0b98878a8f8d
DariusTarvydas <tarvydasdarius00@gmail.com>
2022-05-30 22:05:23 +0300

    test

--- todo-list-shareable-frontend/src/components/NavBar.vue
+++ todo-list-shareable-frontend/src/components/NavBar.vue
@@ -1,9 +1,6 @@
 <template>
   <nav class="square colour">
     <div>
-      <a href="/">
-        <img class="title" src="src/assets/logo.png">
-      </a>
       <h1 class="title"> TODO Sharable</h1>
       <a class="btn" href="/login">Login</a>
       <a class="btn" href="/register">Register</a>
