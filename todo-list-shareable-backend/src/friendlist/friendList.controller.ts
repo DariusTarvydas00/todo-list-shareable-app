9f36f765838cecf4299e3ba98986d9ac7dd71aa1
DariusTarvydas <tarvydasdarius00@gmail.com>
2022-06-13 23:23:07 +0300

    test

--- todo-list-shareable-backend/src/friendlist/friendList.controller.ts
+++ todo-list-shareable-backend/src/friendlist/friendList.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Get, Query} from '@nestjs/common';
 
 @Controller('friendList')
-export class FriendListController {}
+export class FriendListController {
+    @Get()
+    getTasks(){
+        return true;
+    }
+}
