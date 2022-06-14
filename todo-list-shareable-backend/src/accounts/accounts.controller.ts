3d28dd940dd546c90d1eee4d8a44e7f81127602f
DariusTarvydas <tarvydasdarius00@gmail.com>
2022-06-14 14:45:38 +0300

    test

--- todo-list-shareable-backend/src/accounts/accounts.controller.ts
+++ todo-list-shareable-backend/src/accounts/accounts.controller.ts
@@ -1,11 +1,13 @@
-import {Controller, Get, Logger, Query} from '@nestjs/common';
+import {Controller, Get, Query, UseGuards} from '@nestjs/common';
 import {GetTasksFilterDto} from "../tasks/dto/get-tasks-filter.dto";
 import {GetUser} from "../auth/get-user.decorator";
 import {User} from "../core/user.entity";
 import {AccountsService} from "./accounts.service";
 import {Accounts} from "../core/accounts.entity";
+import {AuthGuard} from "@nestjs/passport";
 
 @Controller('accounts')
+@UseGuards(AuthGuard())
 export class AccountsController {
 
     constructor(private accountsService: AccountsService) {}
