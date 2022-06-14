bec8aa7b045d3f70b19cdd4c47aed2c5ff3ab902
DariusTarvydas <tarvydasdarius00@gmail.com>
2022-06-14 11:09:06 +0300

    test

---
+++ todo-list-shareable-backend/src/accounts/accounts.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import {InjectRepository} from "@nestjs/typeorm";
+import {GetTasksFilterDto} from "../tasks/dto/get-tasks-filter.dto";
+import {AccountsRepository} from "./accounts.repository";
+import {Accounts} from "../core/accounts.entity";
+
+@Injectable()
+export class AccountsService {
+    constructor(
+        @InjectRepository(AccountsRepository)
+        private accountsRepository: AccountsRepository
+    ) {}
+
+    async getAccounts(filterDto: GetTasksFilterDto): Promise<Accounts[]>{
+        return this.accountsRepository.getAccounts(filterDto);
+    }
+}
