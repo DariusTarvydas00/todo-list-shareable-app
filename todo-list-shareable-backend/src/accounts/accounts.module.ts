e9e85f3b268bde427a28bcd079ce844a2d43949b
DariusTarvydas <tarvydasdarius00@gmail.com>
2022-06-14 15:08:25 +0300

    test

--- todo-list-shareable-backend/src/accounts/accounts.module.ts
+++ todo-list-shareable-backend/src/accounts/accounts.module.ts
@@ -3,9 +3,10 @@ import { AccountsController } from './accounts.controller';
 import { AccountsService } from './accounts.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {AccountsRepository} from "./accounts.repository";
+import {UsersRepository} from "../auth/users.repository";
 
 @Module({
-  imports:[TypeOrmModule.forFeature([AccountsRepository])],
+  imports:[TypeOrmModule.forFeature([AccountsRepository, UsersRepository])],
   controllers: [AccountsController],
   providers: [AccountsService]
 })
